dc33876e7e790d7ecabb39faf031135fec338860
anikush-yes <anikush@hotmail.com>
2024-12-03 11:57:30 +0200

    commit

--- books/server.js
+++ books/server.js
@@ -5,15 +5,23 @@ const fs = require('node:fs');
 const app = express();
 const port = 80;
 
+// const domain = 'http://books.localhost/';
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true })); //BodyParser yra biblioteka
 
 
+const top = fs.readFileSync('./html/top.html', 'utf8');
+const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
+  
+
 
 app.get('/create', (req, res) => {
-    const file = fs.readFileSync('./html/create.html', 'utf8');
+    const file = top + fs.readFileSync('./html/create.html', 'utf8') + bottom;
     const template = handlebars.compile(file);
-    const data = {manoKintamasis: 'Labas'};
+    const data = {manoKintamasis: 'Labas',
+      pageTitle: 'Nauja knyga',
+    };
     const html = template(data);
     res.send(html);
    
