c5cad28e7662a60fe25198234769cfc1a28926ed
anikush-yes <anikush@hotmail.com>
2024-12-04 12:04:40 +0200

    commit

--- books/server.js
+++ books/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const handlebars = require('handlebars');
+const { v4: uuidv4 } = require('uuid');
 const fs = require('node:fs');
 const app = express();
 const port = 80;
 
-// const domain = 'http://books.localhost/';
+const domain = 'http://books.final/';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true })); //BodyParser yra biblioteka
@@ -16,11 +17,16 @@ const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
   
 
 
+
+
+
+
 app.get('/create', (req, res) => {
     const file = top + fs.readFileSync('./html/create.html', 'utf8') + bottom;
     const template = handlebars.compile(file);
     const data = {manoKintamasis: 'Labas',
       pageTitle: 'Nauja knyga',
+      domain: domain,
     };
     const html = template(data);
     res.send(html);
@@ -31,13 +37,33 @@ app.get('/create', (req, res) => {
 // const template = Handlebars.compile("Name: {{name}}");
 // console.log(template({ name: "Nils" }));
 
+
+
+
+app.post('/store', (req, res) => {
+
+  const { title, author, year, genre, isbn, pages } = req.body;
+  const id = uudv4();
+
+// need validation
+
+  const book =  {id, title, author, year, genre, isbn, pages };
+
+  let data = fs.readFileSync('./data/books.json', 'utf8');
+  data = JSON.parse(data);
+  data.push(book);
+  data = JSON.stringify(data);
+  fs.writeFileSync('./data/books.json', data);
+
+res.status(302).redirect(domain);
+
+})
+
+
+
 app.listen(port, () => {
-    console.log(`Knygynas darbui pasiruošęs ant ${port} porto!`);
+  console.log(`Knygynas darbui pasiruošęs ant ${port} porto!`);
 });
-
-
-
-
 
 
 
