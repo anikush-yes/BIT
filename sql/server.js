76adb1f96ae491c56b546bbd72c86dd1fa36cc4f
anikush-yes <anikush@hotmail.com>
2025-01-03 09:10:06 +0200

    modified server.js in sql
    Signed-off-by: anikush-yes <anikush@hotmail.com>

--- sql/server.js
+++ sql/server.js
@@ -8,8 +8,6 @@ app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
-
 const con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -26,21 +24,23 @@ const con = mysql.createConnection({
 //     console.log('Prisijungėme prie DB');
 // });
 
-
+// app.post('/form-test', (req, res) => {
+//     const titles = req.body.title;
+//     const prices = req.body.price;
+//     console.log(titles);
+//     console.log(prices);
+//     res.send('OK');
+// });
 
 
 app.get('/read', (req, res) => {
 
-    // uzklausa SQL duomenu bazei:
-
     const sql = `
-
-    SELECT id, name,height, type 
-    FROM trees
-    -- WHERE type = 'Lapuotis'OR type = 'Spygliuotis'
-    -- ORDER BY name, height DESC
-    -- LIMIT 3, 2
-
+        SELECT id, name, height, type
+        FROM trees
+        -- WHERE height > 10 AND type = 'Spygliuotis'
+        -- ORDER BY name, height DESC
+        -- LIMIT 3, 3
     `;
 
     con.query(sql, (err, data) => {
@@ -54,29 +54,87 @@ app.get('/read', (req, res) => {
 
 });
 
+
 app.post('/create', (req, res) => {
- 
+
     // INSERT INTO table_name (column1, column2, column3, ...)
     // VALUES (value1, value2, value3, ...);
- 
+
+    // DAryti saugiai, nes galima injekcija
+
     const sql = `
         INSERT INTO trees (name, height, type)
-        VALUES ('${req.body.name}', ${req.body.height}, '${req.body.type}')
+        VALUES (?, ?, ?)
     `;
- 
-    con.query(sql, (err, data) => {
+
+    con.query(sql, [req.body.name, req.body.height, req.body.type], (err) => {
         if (err) {
             res.send('Klaida įrašant duomenis');
             return;
         }
         res.send('OK');
     });
- 
- 
+
+
 });
 
+// DELETE FROM table_name WHERE condition;
+// DELETE FROM trees WHERE id = 123 OR 1;
+
+app.delete('/delete/:id', (req, res) => {
+    
+    // const sql = `
+    //     DELETE FROM trees
+    //     WHERE id = ${req.params.id}
+    // `;
+
+    // con.query(sql, (err) => {
+    //     if (err) {
+    //         res.send('Klaida trinant duomenis');
+    //         return;
+    //     }
+    //     res.send('OK');
+    // });
+
+    // secure delete
+
+    const sql = `
+        DELETE FROM trees
+        WHERE id = ?
+    `;
+
+    con.query(sql, [req.params.id], (err) => {
+        if (err) {
+            res.send('Klaida trinant duomenis');
+            return;
+        }
+        res.send('OK');
+    });
 
 
+});
+
+// UPDATE table_name
+// SET column1 = value1, column2 = value2, ...
+// WHERE condition;
+
+app.put('/update/:id', (req, res) => {
+
+    const sql = `
+        UPDATE trees
+        SET height = ?, name = ?
+        WHERE id = ?
+    `;
+
+    con.query(sql, [req.body.height, req.body.name, req.params.id], (err) => {
+        if (err) {
+            res.send('Klaida atnaujinant duomenis');
+            return;
+        }
+        res.send('OK');
+    });
+
+});
 
 
 
